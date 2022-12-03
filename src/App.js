2f3e5eaf7a665703330fc303d64c2855fa46466f
flying-solo <flyingsoloharshit@gmail.com>
2022-12-03 17:58:45 +0530

    animation fm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useRef } from "react";
+import "./App.css";
 
 function App() {
+  const app = useRef(null);
+  useEffect(() => {
+    const scroller = app.current.addEventListener("wheel", (e) => {
+      console.log(app.current.scrollLeft);
+      if (e.deltaY >= 0 && app.current.getBoundingClientRect().top === 0) {
+        app.current.scrollLeft += e.deltaY;
+      } else if (e.deltaY < 0 && app.current.scrollLeft !== 0) {
+        e.preventDefault();
+        app.current.scrollLeft += e.deltaY;
+      }
+    });
+
+    return () => {
+      app.current.removeEventListener("wheel", scroller);
+    };
+  }, [app.current]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="div1">
+        Div Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        delectus nihil eum consequuntur corrupti, quasi nesciunt corporis enim
+        nostrum expedita possimus saepe laborum repellat magnam dolorum,
+        necessitatibus porro quos perspiciatis culpa molestiae voluptatibus illo
+        voluptas harum numquam! Ipsum laborum autem aut? Laborum dolores
+        molestiae cumque enim, delectus reprehenderit qui laboriosam.
+      </div>
+      <div className="div2" ref={app}>
+        <div className="div21">Div 1</div>
+        <div className="div22">Div 2</div>
+      </div>
     </div>
   );
 }
